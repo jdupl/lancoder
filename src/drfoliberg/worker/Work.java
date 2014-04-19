ef93d468454dec1786b6981a772afaea0494b4d8
drfoliberg <drfoliberg@gmail.com>
2014-04-19 18:02:48 -0400

    (worker) call ffmpeg and read stderr

--- src/drfoliberg/worker/Work.java
+++ src/drfoliberg/worker/Work.java
@@ -1,6 +1,12 @@
 package drfoliberg.worker;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import drfoliberg.common.task.Task;
 
@@ -19,26 +25,52 @@ public class Work extends Thread {
 	@Override
 	public void run() {
 		try {
+			// ffmpeg -i ~/encoding/input.mkv -c:v libx264 -b:v 1000k -strict -2
+			// ~/encoding/output.mkv
 			System.out.println("WORKER WORK THREAD: Executing a task!");
-			sleep(5000);
-			this.task.setProgress(10);
-			sleep(5000);
-			this.task.setProgress(20);
-			sleep(5000);
-			this.task.setProgress(30);
-			sleep(5000);
-			this.task.setProgress(40);
-			sleep(5000);
-			this.task.setProgress(50);
-			sleep(5000);
-			this.task.setProgress(80);
-			sleep(5000);
-			this.task.setProgress(90);
-			System.out.println("WORKER WORK THREAD: Done! Calling back the worker");
+			File f = new File("/home/justin/encoding/output.mkv");
+			if (f.exists()) {
+				System.err.printf("File %s exists ! deleting file...\n",
+						f.getAbsoluteFile());
+				if (!f.delete()) {
+					System.err.printf("Could not delete file %s ",
+							f.getAbsoluteFile());
+					throw new IOException();
+				} else {
+					System.err.println("Success deleting file");
+				}
+			}
+			// TODO Get parameters from the task and bind parameters to process
+			Process process = Runtime
+					.getRuntime()
+					.exec("ffmpeg -i "
+							+ "/home/justin/encoding/input.mkv -c:v libx264 -b:v 1000k "
+							+ "-strict -2 /home/justin/encoding/output.mkv");
+
+			// Read from ffmpeg stderr to get progress
+			InputStream stderr = process.getErrorStream();
+			Scanner s = new Scanner(stderr);
+			String line = "";
+
+			Pattern currentFramePattern = Pattern.compile("frame=\\s*([0-9]*)");
+			Pattern fpsPattern = Pattern.compile("fps=\\s*([0-9]*)");
+			while (s.hasNext()) {
+				line = s.nextLine();
+				Matcher m = currentFramePattern.matcher(line);
+				if (m.find()) {
+					System.err.printf("frame: %s \n", m.group(1));
+				}
+				m = fpsPattern.matcher(line);
+				if (m.find()) {
+					System.err.printf("fps: %s \n", m.group(1));
+				}
+			}
+			System.out.println("Scanner closed");
+			s.close();
+
 			callback.taskDone(task, masterIp);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-//			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 			System.out.println("Work was interrupted!");
 		}
 	}
