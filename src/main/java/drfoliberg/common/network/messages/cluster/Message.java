814b7cd534f9245a7e2930aa26fc9cb016b20a86
Justin Duplessis <drfoliberg@gmail.com>
2014-07-09 21:01:48 -0400

    sort members of class Message

--- src/main/java/drfoliberg/common/network/messages/cluster/Message.java
+++ src/main/java/drfoliberg/common/network/messages/cluster/Message.java
@@ -4,16 +4,16 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
 
-	public String getPath() {
-		return path;
-	}
-
 	private static final long serialVersionUID = -483657531000641905L;
 
 	protected String path;
 
 	public Message(String path) {
 		this.path = path;
+	}
+
+	public String getPath() {
+		return path;
 	}
 
 }
