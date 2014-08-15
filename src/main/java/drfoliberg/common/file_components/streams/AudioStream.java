e667fe4c25581f5f2fe1b4756c31180eb1ff59c2
Justin Duplessis <drfoliberg@gmail.com>
2014-08-15 00:06:24 -0400

    Implement audiostream detection

--- src/main/java/drfoliberg/common/file_components/streams/AudioStream.java
+++ src/main/java/drfoliberg/common/file_components/streams/AudioStream.java
@@ -1,5 +1,6 @@
 package drfoliberg.common.file_components.streams;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class AudioStream extends Stream {
@@ -10,5 +11,29 @@ public class AudioStream extends Stream {
 
 	public AudioStream(JsonObject json) {
 		super(json);
+		JsonElement element = null;
+		if ((element = json.get("bit_rate")) != null) {
+			// convert from bit/s to kbps
+			this.bitrate = element.getAsInt() / 1000;
+		}
+		if ((element = json.get("channels")) != null) {
+			this.channels = element.getAsInt();
+		}
+		if ((element = json.get("sample_rate")) != null) {
+			this.sampleRate = element.getAsInt();
+		}
 	}
+
+	public float getBitrate() {
+		return bitrate;
+	}
+
+	public int getChannels() {
+		return channels;
+	}
+
+	public int getSampleRate() {
+		return sampleRate;
+	}
+
 }
