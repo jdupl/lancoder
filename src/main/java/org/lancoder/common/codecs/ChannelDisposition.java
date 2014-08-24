482a213680a428c0d8122c9ea7a9e1f7f40dd82f
Justin Duplessis <drfoliberg@gmail.com>
2014-08-24 18:47:32 -0400

    ChannelDisposition: rename auto to original

--- src/main/java/org/lancoder/common/codecs/ChannelDisposition.java
+++ src/main/java/org/lancoder/common/codecs/ChannelDisposition.java
@@ -2,7 +2,7 @@ package org.lancoder.common.codecs;
 
 public enum ChannelDisposition {
 	/**
-	 * Auto is used only to keep original channel arrangement
+	 * ORIGINAL is used only to keep original channel arrangement
 	 */
 	ORIGINAL(0), MONO(1), STEREO(2), SURROUND_51(6), SURROUND_71(8);
 
