bf4e0abea0fd25324a6bbe100241558126c68989
Justin Duplessis <drfoliberg@gmail.com>
2014-09-19 20:45:22 -0400

    Implement basic messager to send Message to simplify exchanges

---
+++ src/main/java/org/lancoder/common/network/Messager.java
@@ -0,0 +1,33 @@
+package org.lancoder.common.network;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownServiceException;
+
+import org.eclipse.jetty.http.HttpTester.Message;
+
+public class Messager {
+
+	private final static int DEFAULT_TIMEOUT_MSEC = 2000;
+
+	public static Message send(InetSocketAddress addr, Message m) throws ClassNotFoundException, IOException {
+		try (Socket s = new Socket()) {
+			s.setSoTimeout(DEFAULT_TIMEOUT_MSEC);
+			s.connect(addr);
+			ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+			out.writeObject(m);
+			out.flush();
+			Object res = in.readObject();
+			if (res instanceof Message) {
+				return (Message) res;
+			} else {
+				throw new UnknownServiceException(String.format("Response from %s is not a valid message.",
+						addr.getAddress()));
+			}
+		}
+	}
+}
