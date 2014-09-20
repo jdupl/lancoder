7e50b76a190e3c5647c664644ebfa4d5085f54bc
Justin Duplessis <drfoliberg@gmail.com>
2014-09-19 20:11:00 -0400

    Refactor config factory handling of default path.

--- src/main/java/org/lancoder/ConfigFactory.java
+++ src/main/java/org/lancoder/ConfigFactory.java
@@ -17,9 +17,69 @@ import com.google.gson.Gson;
 public class ConfigFactory<T extends Config> {
 
 	private Class<T> clazz;
+	private String configPath;
 
 	public ConfigFactory(Class<T> clazz) {
 		this.clazz = clazz;
+		configPath = generate().getDefaultPath();
+	}
+
+	public ConfigFactory(Class<T> clazz, String configPath) {
+		this.clazz = clazz;
+		this.configPath = configPath;
+	}
+
+	/**
+	 * Initialize a new configuration and save new configuration to disk.
+	 * 
+	 * @param userInput
+	 *            If true, the method will get data from System.in (the user). Otherwise loads default values.
+	 * @return The new configuration
+	 */
+	public T init(boolean userInput) {
+		T config = userInput ? promptUser() : generate();
+		config.setConfigPath(configPath);
+		config.dump();
+		return config;
+	}
+
+	/**
+	 * Load configuration from disk from the provided path.
+	 * 
+	 * @param clazz
+	 *            The type of configuration
+	 * @return The loaded configuration
+	 * @throws MissingConfiguration
+	 *             If file is corrupted or missing
+	 */
+	public T load() throws MissingConfiguration {
+		T config = generate();
+		if (!Files.exists(Paths.get(configPath))) {
+			throw new MissingConfiguration();
+		}
+		try {
+			byte[] b = Files.readAllBytes(Paths.get(configPath));
+			config = fromJson(new String(b, "UTF-8"));
+			config.setConfigPath(configPath);
+			System.out.println("Loaded config from disk");
+		} catch (IOException e) {
+			throw new MissingConfiguration();
+		}
+		return config;
+	}
+
+	/**
+	 * Generate default config instance
+	 * 
+	 * @return The default config
+	 */
+	private T generate() {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	private HashMap<Field, String> getFields() {
@@ -60,100 +120,9 @@ public class ConfigFactory<T extends Config> {
 		return config;
 	}
 
-	/**
-	 * Generate default config instance
-	 * 
-	 * @return The default config
-	 */
-	private T generate() {
-		try {
-			return clazz.newInstance();
-		} catch (InstantiationException | IllegalAccessException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	/**
-	 * Initialize a new configuration and save new configuration to disk with default path.
-	 * 
-	 * @param clazz
-	 *            The type of configuration
-	 * @param userInput
-	 *            If true, the method will get data from System.in (the user). Otherwise loads default values.
-	 * @return The new configuration
-	 */
-	public T init(boolean userInput) {
-		T config = null;
-		try {
-			config = clazz.newInstance();
-			return init(config.getDefaultPath(), userInput);
-		} catch (InstantiationException | IllegalAccessException e) {
-			return null;
-		}
-	}
-
-	/**
-	 * Initialize a new configuration and save new configuration to disk.
-	 * 
-	 * @param configPath
-	 *            The path of the configuration
-	 * @param clazz
-	 *            The type of configuration
-	 * @param userInput
-	 *            If true, the method will get data from System.in (the user). Otherwise loads default values.
-	 * @return The new configuration
-	 */
-	public T init(String configPath, boolean userInput) {
-		T config = userInput ? promptUser() : generate();
-		config.setConfigPath(configPath);
-		config.dump();
-		return config;
-	}
-
-	/**
-	 * Load configuration from disk from the default class path.
-	 * 
-	 * @param clazz
-	 *            The type of configuration
-	 * @return The loaded configuration
-	 * @throws MissingConfiguration
-	 *             If file is corrupted or missing
-	 */
-	public T load() throws MissingConfiguration {
-		T config = generate();
-		return load(config.getDefaultPath());
-	}
-
-	/**
-	 * Load configuration from disk from the provided path.
-	 * 
-	 * @param configPath
-	 *            The path of the configuration
-	 * @param clazz
-	 *            The type of configuration
-	 * @return The loaded configuration
-	 * @throws MissingConfiguration
-	 *             If file is corrupted or missing
-	 */
-	public T load(String configPath) throws MissingConfiguration {
-		T config = generate();
-		if (!Files.exists(Paths.get(configPath))) {
-			throw new MissingConfiguration();
-		}
-		try {
-			byte[] b = Files.readAllBytes(Paths.get(configPath));
-			config = fromJson(new String(b, "UTF-8"));
-			config.setConfigPath(configPath);
-			System.out.println("Loaded config from disk");
-		} catch (IOException e) {
-			throw new MissingConfiguration();
-		}
-		return config;
-	}
-
 	private T fromJson(String jsonString) {
 		Gson gson = new Gson();
 		return gson.fromJson(jsonString, this.clazz);
 	}
+
 }
