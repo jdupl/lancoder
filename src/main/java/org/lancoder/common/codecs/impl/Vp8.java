3032a1a7bcdfeab09a832ecd9abe442f8461cf58
Justin Duplessis <drfoliberg@gmail.com>
2014-12-21 15:23:20 -0500

    VP9 and VP8 extented wrong class

--- src/main/java/org/lancoder/common/codecs/impl/Vp8.java
+++ src/main/java/org/lancoder/common/codecs/impl/Vp8.java
@@ -1,9 +1,9 @@
 package org.lancoder.common.codecs.impl;
 
 import org.lancoder.common.codecs.CodecEnum;
-import org.lancoder.common.codecs.base.AudioCodec;
+import org.lancoder.common.codecs.base.VideoCodec;
 
-public class Vp8 extends AudioCodec {
+public class Vp8 extends VideoCodec {
 
 	private static final long serialVersionUID = -3179666990115591649L;
 
--- src/main/java/org/lancoder/common/codecs/impl/Vp9.java
+++ src/main/java/org/lancoder/common/codecs/impl/Vp9.java
@@ -1,9 +1,9 @@
 package org.lancoder.common.codecs.impl;
 
 import org.lancoder.common.codecs.CodecEnum;
-import org.lancoder.common.codecs.base.AudioCodec;
+import org.lancoder.common.codecs.base.VideoCodec;
 
-public class Vp9 extends AudioCodec {
+public class Vp9 extends VideoCodec {
 
 	private static final long serialVersionUID = -4479335176235957724L;
 
